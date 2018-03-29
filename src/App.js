6afbf11ea59e4626de5ca19fdac13826d31587e4
meryldakin <meryl.k.dakin@gmail.com>
2018-03-29 11:47:25 -0400

    merge

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ class App extends Component {
           e: gifInfo.data.embed_url
         }, "squeaky")
 
-<<<<<<< HEAD
         fetch(`https://www.googleapis.com/urlshortener/v1/url?key=${GOOGLE_API_KEY}`, {
           method: "POST",
           headers: {
@@ -44,83 +43,69 @@ class App extends Component {
         })
           .then(res => res.json())
           .then(res => {
+            this.setState({
+              shareURL: res.id,
+              scrambled,
+              motivationalPhrase: phrase,
+              searchTerm: searchTerm,
+              embedURL: gifInfo.data.embed_url
+            })
+          })
+      })
+  }
 
-=======
-				fetch(`https://www.googleapis.com/urlshortener/v1/url?key=${GOOGLE_API_KEY}`, {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json"
-					},
-					body: JSON.stringify({
-						"longUrl": `${MY_URL}/${shareHash}`
-					})
-				})
-					.then(res => res.json())
-					.then(res => {
-						this.setState({
-							shareURL: res.id,
-							scrambled,
-							motivationalPhrase: phrase,
-							searchTerm: searchTerm,
-							embedURL: gifInfo.data.embed_url
-						})
-					})
-			})
-	}
->>>>>>> ab013dbb0e56c37c940ae946a7bc98c94dee81a6
+  shuffle = (a) => {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
+  getMotivationalWord = () => {
+    let word = this.shuffle(motivationArray)[0]
+    let wordArray = word.split("")
 
-            shuffle = (a) => {
-              for (let i = a.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [a[i], a[j]] = [a[j], a[i]];
-              }
-              return a;
-            }
-            getMotivationalWord = () => {
-              let word = this.shuffle(motivationArray)[0]
-              let wordArray = word.split("")
+    return wordArray.join(" · ").toUpperCase()
+  }
 
-              return wordArray.join(" · ").toUpperCase()
-            }
-
-            componentDidMount() {
-              let token = window.location.pathname.replace('/', "")
-              let decoded;
-              if (token) {
-                try {
-                  decoded = jwt.verify(token, "squeaky")
+  componentDidMount() {
+    let token = window.location.pathname.replace('/', "")
+    let decoded;
+    if (token) {
+      try {
+        decoded = jwt.verify(token, "squeaky")
 
 
 
-                  this.setState({
-                    scrambled: decoded.s,
-                    embedURL: decoded.e,
-                    shareURL: "",
-                    searchTerm: decoded.t,
-                    motivationalPhrase: decoded.p
-                  }, this.fetchGif)
-                } catch (error) {
-                  this.changeTerm("The Squeaky Mouse", "The Squeaky Mouse")
-                }
-              } else {
-                this.changeTerm("The Squeaky Mouse", "The Squeaky Mouse")
-              }
-            }
+        this.setState({
+          scrambled: decoded.s,
+          embedURL: decoded.e,
+          shareURL: "",
+          searchTerm: decoded.t,
+          motivationalPhrase: decoded.p
+        }, this.fetchGif)
+      } catch (error) {
+        this.changeTerm("The Squeaky Mouse", "The Squeaky Mouse")
+      }
+    } else {
+      this.changeTerm("The Squeaky Mouse", "The Squeaky Mouse")
+    }
+  }
 
-            render() {
+  render() {
 
-              return (
-                <div className="App">
+    return (
+      <div className="App">
 
-                  <Generator embedURL={this.state.embedURL}
-                    motivationalPhrase={this.state.motivationalPhrase}
-                    changeTerm={this.changeTerm}
-                    shareURL={this.state.shareURL}
-                    scrambled={this.state.scrambled} />
-                </div>
-              );
-            }
-          }
+        <Generator embedURL={this.state.embedURL}
+          motivationalPhrase={this.state.motivationalPhrase}
+          changeTerm={this.changeTerm}
+          shareURL={this.state.shareURL}
+          scrambled={this.state.scrambled} />
+      </div>
+    );
+  }
+}
 
 export default App;
 
